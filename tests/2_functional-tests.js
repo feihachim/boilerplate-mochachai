1776c2ab2093975a5a247d1250b1c4becb8db6fd
Hachim YOUSSOUFA <feihachim@yahoo.fr>
2023-01-23 13:12:14 +0100

    updated browser site

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -63,7 +63,7 @@ suite('Functional Tests', function () {
 });
 
 const Browser = require('zombie');
-Browser.site = 'http://0.0.0.0:3000';
+Browser.site = 'https://mochachai-feihachim.onrender.com';
 suite('Functional Tests with Zombie.js', function () {
   this.timeout(5000);
   const browser = new Browser();
